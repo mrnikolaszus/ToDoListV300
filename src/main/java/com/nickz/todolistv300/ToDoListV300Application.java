67d0dbd98822d653aa0b87221910e5fcdd181e35
Nikolas <9225407@gmail.com>
2024-02-24 23:50:57 +0300

    Initial commit

---
+++ src/main/java/com/nickz/todolistv300/ToDoListV300Application.java
@@ -0,0 +1,13 @@
+package com.nickz.todolistv300;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListV300Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListV300Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nickz/todolistv300/ToDoListV300ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickz.todolistv300;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListV300ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
